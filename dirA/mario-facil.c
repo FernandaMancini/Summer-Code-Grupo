af86cfeaa4b5a7a3b6949bc407bfca7fe6e372c8
FernandaMancini <108295414+FernandaMancini@users.noreply.github.com>
2023-01-07 19:43:51 +0000

    Sat, Jan 7, 2023, 7:43 PM +00:00

--- dirA/mario-facil.c
+++ dirA/mario-facil.c
@@ -3,7 +3,7 @@
 
 int main (void)
 {
-    int height, i, j;
+    int height, i, j, space;
     do
     {
         height = get_int ("Altura: ");
@@ -12,6 +12,9 @@ int main (void)
     for (i = 0; i < height; i++)
     {
         for (space = 0; space < height - j; )
+        {
+        printf ("  ");
+        }
         for (j = 0; j <= i; j++)
         {
             printf ("#");
