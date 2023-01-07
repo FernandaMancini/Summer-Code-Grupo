d3a8cc10316ebb6fa51aa49089229c9306c74be4
FernandaMancini <108295414+FernandaMancini@users.noreply.github.com>
2023-01-07 17:31:26 +0000

    Sat, Jan 7, 2023, 5:31 PM +00:00

---
+++ dirA/mario-dificil.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    int height, i, j;
+    do
+    {
+        height = get_int ("Altura: ");
+    }
+    while (height < 1 || height > 8);
+    for (i = 0; i < height; i++)
+    {
+        for (j = 0; j <= i; j++)
+        {
+            printf ("#" + " " + "#");
+        }
+
+        printf ("\n");
+    }
+}
