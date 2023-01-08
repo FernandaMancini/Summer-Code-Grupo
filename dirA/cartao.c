0baf233000a9cf31284ab482ec45684574efa82b
FernandaMancini <108295414+FernandaMancini@users.noreply.github.com>
2023-01-08 18:43:03 +0000

    Sun, Jan 8, 2023, 6:43 PM +00:00

--- dirA/cartao.c
+++ dirA/cartao.c
@@ -1,40 +1,82 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(void)
 {
-   long n;
-   do
-   {
-      n = get_long("Numero do cartao: ");
-   } while (n <= 0);
+    long creditCardNumber;
 
-   long cc = n;
-   int soma;
-   int count = 0;
+    do
+    {
+        creditCardNumber = get_long ("Numero do cartao: ");
+    }
+    while (creditCardNumber <= 0);
 
-   while (cc > 0)
-   {
-      int ultdig = cc % 10;
-      soma = soma + ultdig;
-      cc = cc/100;
-   }
+    long workingCC = creditCardNumber;
+    int sum = 0;
+    int count = 0;
+    long divisor = 10;
+    char result[11];
 
-   cc = n/10;
-   while (cc > 0)
-   {
-      int ultdig = cc % 10;
-      int vezesdois = ultdig * 2;
-      soma = soma + (vezesdois /10);
-      cc = cc/100;
-   }
-   cc = n;
-   while (cc != 0)
-   {
-      cc = cc/10;
-      count ++;
-   }
-   printf ("%i\n", count);
+    // 1st case
+    while (workingCC > 0)
+    {
+        int lastDigit = workingCC % 10;
+        sum = sum + lastDigit;
+        workingCC = workingCC / 100;
+    }
 
+    // 2nd case
+    workingCC = creditCardNumber / 10;
+    while (workingCC > 0)
+    {
+        int lastDigit = workingCC % 10;
+        int timesTwo = lastDigit * 2;
+        sum = sum + (timesTwo % 10) + (timesTwo / 10);
+        workingCC = workingCC / 100;
+    }
 
+    // length of the number / digit count
+    workingCC = creditCardNumber;
+    while (workingCC != 0)
+    {
+        workingCC = workingCC / 10;
+        count++;
+    }
+
+    // divisor
+    for (int i = 0; i < count - 2; i++)
+    {
+        divisor = divisor * 10;
+    }
+
+    int firstDigit = creditCardNumber / divisor;
+    int firstTwoDigits = creditCardNumber / (divisor / 10);
+
+    // final checks
+    if (sum % 10 == 0)
+    {
+        if (firstDigit == 4 && (count == 13 || count == 16))
+        {
+            strcpy(result, "VISA");
+        }
+        else if ((firstTwoDigits == 34 || firstTwoDigits == 37) && count == 15)
+        {
+            strcpy(result, "AMEX");
+        }
+        else if ((50 < firstTwoDigits && firstTwoDigits < 56) && count == 16)
+        {
+            strcpy(result, "MASTERCARD");
+        }
+        else
+        {
+            strcpy(result, "INVALID");
+        }
+    }
+    else
+    {
+        strcpy(result, "INVALID");
+    }
+
+    printf("%s\n", result);
 }
