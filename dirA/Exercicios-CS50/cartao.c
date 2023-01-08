aef6c7ddf1b60890717b23751766cfa1ac3af9e4
FernandaMancini <108295414+FernandaMancini@users.noreply.github.com>
2023-01-08 17:18:10 +0000

    Sun, Jan 8, 2023, 5:18 PM +00:00

--- dirA/Exercicios-CS50/cartao.c
+++ dirA/Exercicios-CS50/cartao.c
@@ -3,5 +3,17 @@
 
 int main(void)
 {
+   long n = get_long("Numero do cartao: ");
+   int i = 0;
+   cc long = n;
+   while (cc > 0)
+   {
+    cc = cc/10;
+    i++;
+   }
+   if (i != 13 && i != 15 && i != 16)
+   {
+    printf ("INVALIDO! \n");
+   }
    
 }
