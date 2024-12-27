3c016f15a4fc303649db9d2dbce83d892fdd2657
Fernanda Alexandre Mancini <108295414+FernandaMancini@users.noreply.github.com>
2024-12-27 11:14:48 -0300

    Delete SQL/Movies/10a.sql

--- SQL/Movies/10a.sql
+++
@@ -1,6 +0,0 @@
-SELECT DISTINCT name
-FROM people
-JOIN directors ON people.id = directors.person_id
-JOIN movies ON movies.id = directors.movie_id
-JOIN ratings ON movies.id = ratings.movie_id
-WHERE rating >= 9.0;
